a469a68975a1c5a02c2ca4faefcd16fdb5cb2e82
isamukoseda <isamu.koseda@gmail.com>
2017-05-25 12:57:38 +0900

    Issue#2対応
    追加したテストパータン全てに対応
    parseInt()をMath.floor()に変更

--- calcPageParam.js
+++ calcPageParam.js
@@ -5,28 +5,34 @@
 function calc_page_param(NCPSApageNO, NCPSAperPAGE, NCPSAcount) {
     
     // get total record
-    var total = NCPSAcount;
-    var per_page = NCPSAperPAGE;
-    var page = NCPSApageNO;
+    var total = Math.floor(NCPSAcount);
+    var per_page = Math.floor(NCPSAperPAGE);
+    var page = Math.floor(NCPSApageNO);
     
     // check per_page value
     if(per_page < 1){
         per_page = 1;
     }
-    if(total < per_page) {
+    if(per_page > total) {
         per_page = total;
     }
     
     // check page value
-    if(parseInt(total/per_page) < page) {
-        page = parseInt(total/per_page);
+    if(per_page * (page -1) > total) {
+        page = Math.floor(total / per_page) +1;
+    } else if (per_page * (page -1) === total) {
+        page = Math.floor(total / per_page);
+    }
+    if(page > total) {
+        page = total;
     }
     if(page < 1) {
         page = 1;
     }
 
     // calc result(INDEX) value    
-    var result = (per_page * (page - 1)) + 1;
+    var result = Math.floor(per_page * (page - 1)) + 1;
+    
     if (result > total) {
         result = total;
     }
